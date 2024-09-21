dd57d76d53ec1bce5c4f9157e59dd113b766fc8c
NehaJainG <nehajaing1324@gmail.com>
2024-09-21 12:42:29 +0530

    init

---
+++ src/main/java/com/nj/HoneyLock/HoneyLockApplication.java
@@ -0,0 +1,13 @@
+package com.nj.HoneyLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyLockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyLockApplication.class, args);
+	}
+
+}
