c16d084655679f2d3dfcb32f44360dc1d8a19d89
NehaJainG <nehajaing1324@gmail.com>
2024-10-20 20:36:44 +0530

    server imple

--- src/main/java/com/nj/HoneyLock/service/OpaqueServer.java
+++ src/main/java/com/nj/HoneyLock/service/OpaqueServer.java
@@ -1,9 +1,12 @@
-package com.nj.HoneyLock.server;
+package com.nj.HoneyLock.service;
 
-import com.nj.HoneyLock.model.User;
-import com.nj.HoneyLock.model.UserRecord;
+import com.nj.HoneyLock.hcServer.HoneyChecker;
+import com.nj.HoneyLock.server.model.User;
+import com.nj.HoneyLock.server.model.UserRecord;
+import com.nj.HoneyLock.server.repo.UserRepository;
 import com.nj.HoneyLock.utils.HoneywordGenerator;
 import com.nj.HoneyLock.utils.Opaque;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigInteger;
 import java.security.KeyPair;
@@ -11,13 +14,16 @@ import java.security.PublicKey;
 import java.util.*;
 
 public class OpaqueServer extends Opaque {
+  @Autowired
+  private UserRepository userRepository;
   KeyPair serverKeys;
   HoneywordGenerator gen;
+  HoneyChecker hc;
 
-
-  OpaqueServer() throws Exception {
+  public OpaqueServer() throws Exception {
     this.serverKeys = ake.generateECKeyPair();
     gen = new HoneywordGenerator(10);
+    hc=new HoneyChecker();
   }
 
 
@@ -33,6 +39,11 @@ public class OpaqueServer extends Opaque {
   private List<String> generateHoneyKeys(String pattern, String secret, String cipher) throws Exception {
     List<String> honeyKeys = new ArrayList<>();
     List<String> honeywords = gen.generateHoneywords(pattern);
+    System.out.println("***************************************************");
+    System.out.println("HONEY WORDS:");
+    display(honeywords);
+    System.out.println("***************************************************");
+
     for (String honeyword : honeywords) {
       BigInteger honeyPrivateKey = he.decrypt(cipher,oprf.generateRW(honeyword,secret));
       PublicKey honeyPublickey = ake.getPublic(ake.retrievePrivateKey(honeyPrivateKey));
@@ -41,7 +52,7 @@ public class OpaqueServer extends Opaque {
     return honeyKeys;
   }
 
-  int suffleKeys(String realKeys, List<String> honeyKeys){
+  int shuffleKeys(String realKeys, List<String> honeyKeys){
     Random rand = new Random(); // Randomly select an index to insert the real key
     int insertIndex = rand.nextInt(honeyKeys.size() + 1);
     honeyKeys.add(insertIndex, realKeys);
@@ -52,37 +63,67 @@ public class OpaqueServer extends Opaque {
     return -1;
   }
 
-  void generateServerSK(String userId){
-    //find user with user id
-    //send the client
-
-  }
-
-  public void saveUserRecord(User user,String pattern) throws Exception {
+  public UserRecord saveUserRecord(User user,String pattern) throws Exception {
     List<String> keys = generateHoneyKeys(pattern, user.getSecret(),user.getCipher());
     String realKeys = user.getCipher();
-    int index = suffleKeys(realKeys,keys);
+    int index = shuffleKeys(realKeys,keys);
     //save the index
-    UserRecord newUser = new UserRecord(user.getId(), user.getName(), user.getCipher(), user.getSecret(), keys);
+    hc.saveUser(user.getUsername(), index);
+    UserRecord newUser = new UserRecord(null, user.getUsername(), user.getName(), user.getCipher(), user.getSecret(), keys);
+    //save the user
+    userRepository.save(newUser);
+    return newUser;
   }
 
-
-
-  public static void main(String[] args) throws Exception {
-    OpaqueServer s = new OpaqueServer();
-    User user = new User("neha",
-      "EBIlZx3rltPz3hI5ZFX1v2SjknrsGbul3OkAkjIRPliX0HdZvm7jPef0DtucPRm0bcJgf6zZuQwEw/EdDobSiQ==",
-      "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE5RV0/l277oayhBvAjxWzC6ePCpVk23bjkQDDOp/iNpfHMdtPlsg1WnPTpPdYarOJS7Y/Gw/iA/Si5wwaXyCRpA==",
-      "1234567890");
-   s.saveUserRecord(user,"ADS");
+  public UserRecord getUserRecord(String username){
+    //find user with user id
+    Optional<UserRecord> user = userRepository.getUserByUsername(username);
+    return user.orElse(null);
   }
-//  void display(ArrayList<ArrayList<String>> l){
-//    System.out.println("---------------------------------------------------------------------------");
-//    System.out.println(l.size());
-//    for(ArrayList<String> list:l){
-//      for(String item : list) System.out.print(item+" ");
-//      System.out.println();
-//    }
-//    System.out.println("---------------------------------------------------------------------------");
+
+  //send cipher and server public key for session key generation
+  public User getUser(User user){
+    UserRecord userRecord = getUserRecord(user.getUserName());
+    user.setCipher(userRecord.getCipher());
+    user.setPublicKey(ake.formatPublicKey(serverKeys.getPublic()));
+    return user;
+  }
+
+  List<byte[]> generateServerSK(UserRecord user) throws Exception {
+    List<byte[]> sessionKeys = new ArrayList<>();
+    for (String publicKey: user.getKeys()) {
+        PublicKey KU = ake.retrievePublicKey(publicKey);
+       byte[] sk = ake.generateSharedSecret(serverKeys.getPrivate(), KU);
+      sessionKeys.add(sk);
+    }
+    return sessionKeys;
+  }
+
+  boolean authenticate(UserRecord user, byte[] clientSK) throws Exception {
+    List<byte[]> serverSKs = generateServerSK(user);
+    int realIndex = hc.getIndex(user.getUserName());
+    for (int i = 0; i < serverSKs.size() ; i++) {
+      if (Arrays.equals(clientSK, serverSKs.get(i))){
+        if(realIndex==i) System.out.println("\nSuccessfully authenticated...\n");
+        else System.out.println("\npassword leakage detected...\n");
+        return true;
+      }
+    }
+    System.out.println("\nAuthentication failed...\n");
+    return false;
+  }
+
+//  public static void main(String[] args) throws Exception {
+//    OpaqueServer s = new OpaqueServer();
+//    User user = new User("neha123","neha",
+//      "EBIlZx3rltPz3hI5ZFX1v2SjknrsGbul3OkAkjIRPliX0HdZvm7jPef0DtucPRm0bcJgf6zZuQwEw/EdDobSiQ==",
+//      "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE5RV0/l277oayhBvAjxWzC6ePCpVk23bjkQDDOp/iNpfHMdtPlsg1WnPTpPdYarOJS7Y/Gw/iA/Si5wwaXyCRpA==",
+//      "1234567890");
+//   s.saveUserRecord(user,"ADS");
 //  }
+  private void display(List<String> list){
+    for (String s : list) {
+      System.out.println(s);
+    }
+  }
 }
